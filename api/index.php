405da8cf2cc48cd4846e0f3623066c64b7f82a40
jngradeepak <110615263+jngradeepak@users.noreply.github.com>
2026-07-31 16:10:23 +0530

    fix: auto-configure vercel environment variables to fix blank page

--- api/index.php
+++ api/index.php
@@ -1,2 +1,15 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+putenv('APP_DEBUG=true');
+
 require __DIR__ . "/../public/index.php";
